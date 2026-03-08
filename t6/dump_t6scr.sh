42eb56d133066eec2f068889dd16ea42ed06c175
Jake-NotTheMuss <jakenotthemuss@gmail.com>
2026-03-08 18:24:38 -0500

    make path to CODMPServer_PC.exe a variable, and add description of the executable used in reflex command

--- t6/dump_t6scr.sh
+++ t6/dump_t6scr.sh
@@ -4,6 +4,22 @@ srcdir=`dirname "$0"`
 
 cd "$srcdir" || exit
 
+exe=CoDMPServer_PC.exe
+
+if test "x$COD_EXE" != "x"; then
+	if test ! -e "$COD_EXE"; then
+		echo "COD_EXE value: $COD_EXE: file does not exist"
+		exit 1
+	fi
+	exe=$COD_EXE
+fi
+
+if test ! -e $exe; then
+	echo "$exe not found in script directory"
+	echo "Define the variable COD_EXE to provide the path to the T6 executable"
+	exit 1
+fi
+
 import_makefile_vars() {
 	file=$1
 	shift
@@ -19,14 +35,26 @@ import_makefile_vars Makefile PYTHON FLEX CC REFLEXDIR
 
 OUTDIR=dump
 
-"$PYTHON" --version
+"$PYTHON" --version || exit
 
-"$PYTHON" "$REFLEXDIR/py/reflex.py" --accept 9076808 2 --base 9078576 2 --chk 9080576 2 --def 9079096 2 --ec 9077312 4 --meta 9078336 4 --nxt 9079616 2 --max-state 248 "C:/projects/research/linker/reflex/t6/CoDMPServer_PC.exe" $OUTDIR || exit
+# Run reflex on the T6 server debug executable, CoDMPServer_PC.exe
+# This executable has:
+#   - SHA256: ecede1035e091aaf45f358f5d7f73aa90f2a747027a13bf9906699d1121736cc
+#   - BUILD_NUMBER: 4
+#   - CHANGELIST_NUMBER: 1757733
+#   - BUILD_TIME: Tue May 06 09:59:33 2014
+# It can be acquired from this mega link:
+# https://mega.nz/#!VUVATCDK!aHbD69iprnTDgvrvwTQD_LZ9fCWUZ06zltZkNhULT1M
+"$PYTHON" "$REFLEXDIR/py/reflex.py" --accept 9076808 2 --base 9078576 2 \
+--chk 9080576 2 --def 9079096 2 --ec 9077312 4 --meta 9078336 4 \
+--nxt 9079616 2 --max-state 248 "$exe" $OUTDIR || exit
 
 dot -Tpdf -o $OUTDIR/out.png $OUTDIR/1.dot || exit
 
-"$PYTHON" "$REFLEXDIR/py/simplify.py" "$OUTDIR/G.gpickle" "$OUTDIR/simple/" || exit
+"$PYTHON" "$REFLEXDIR/py/simplify.py" "$OUTDIR/G.gpickle" \
+"$OUTDIR/simple/" || exit
 
 sh "$REFLEXDIR/subs2pdf.sh" $OUTDIR/simple || exit
 
-powershell -File "$srcdir/../jreflex.ps1" -Name "../dumptable/t6.txt" -Dir "$OUTDIR" -Reflex "$REFLEXDIR" || exit
+powershell -File "$srcdir/../jreflex.ps1" -Name "../dumptable/t6.txt" -Dir \
+"$OUTDIR" -Reflex "$REFLEXDIR" || exit
